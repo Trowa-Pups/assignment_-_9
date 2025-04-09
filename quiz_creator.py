9d5386112918e88cd90a6a55bef8da144f4963ea
unknown <sainttrowagmail.com>
2025-04-09 21:01:45 +0800

    pseudocode add

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,6 @@
-#Import a library or make a program to create or edit a file
+#Import or make a program that creates or edit a file or something that checks a file
 #Ask the user to input what quiz number they want to create or edit
 #Ask the user to input their question 
 #Ask the user to input four options, and input the correct answer
+#Print the input of the user in the text file
 #If the user inputs "exit", make the program break and update the file with the new inputs of the user
