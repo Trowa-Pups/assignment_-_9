629608ec59679b60c1f0ee5c9a388d06782d810f
unknown <sainttrowagmail.com>
2025-04-10 13:14:13 +0800

    Using os.path.join to join the paths, so thats the program could find the file that the user inputted

--- quiz_creator.py
+++ quiz_creator.py
@@ -3,9 +3,10 @@ import os #using import os to check files in the computer
 
 #Ask the user to input what quiz number they want to create or edit
 quiz_number = str(input("Please input what quiz you are going to edit or create Ex.(quiz_#_1.txt): "))
-
-if os.path.exists(quiz_number):
-    print(f"{quiz_number}" + "will be opened")
+downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads") #Using expanduser("~") to make it search home directory and put "Downloads" to find the downlaods folder, and using os.path.join to combines all the paths taken to make it one path
+file_path = os.path.join(downloads_folder, quiz_number) #Joining both of them to make the full file path
+if os.path.exists(file_path): #To check if the file is there or not
+    print(f"{quiz_number}" + " will be opened")
 
 else:
     print(f"{quiz_number}" + " does not exist")
