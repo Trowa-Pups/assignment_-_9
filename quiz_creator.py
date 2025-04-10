595ccbce698b3761d3c11c9e505452d915e750b9
unknown <sainttrowagmail.com>
2025-04-10 17:40:50 +0800

    Finished the multiple choice, and the right answer, putting a list also to store in the right answers and write it after entering exit. Also changed if path does exist into 'a' to not erase previous text

--- quiz_creator.py
+++ quiz_creator.py
@@ -10,44 +10,76 @@ file_path = os.path.join(downloads_folder, quiz_number) #Joining both of them to
 
 if os.path.exists(file_path): #To check if the file is there or not
     print(f"{quiz_number}" + " will be opened")
-    with open(file_path, "w") as file: #Opens the file and using "w", it is the 'write' mode to put texts in the txt file
+    with open(file_path, "a") as file: #Opens the file and it uses 'a' for it doesn't remove previous texts in the file
         i = 0 
+
+        right_answers_list = [] #To store the right answers
+
         while True: #To make it a loop so that the user can put how much questions they want in the file
-            question = str(input(f"Please input the Question no.{i + 1}, if done, type 'exit': ")) #Ask the user to input their question 
+            question = str(input(f"Please input the new question, if done, type 'exit': ")) #Ask the user to input their question 
 
             if question.lower() == "exit": #If the user inputs "exit", make the program break and update the file with the new inputs of the user
-                print("Edit is done")
+                file.write(f"List of right answers: {right_answers_list}") #To write the list after the user exits
+                print("Exiting...")
                 break
 
+            #Ask the user to input four options, and input the correct answer
             option_a = input("Input option A of the question: ")
             option_b = input("Input option B of the question: ")
             option_c = input("Input option C of the question: ")
             option_d = input("Input option D of the question: ")
 
             if option_a == option_b or option_a == option_c or option_a == option_d or option_b == option_c or option_b == option_d or option_c == option_d:
-                print("Same answer detected! Do you want to continue?")
+                same_answer_detector = str(input("Same answer detected! Do you want to continue? Yes/No: "))
+                if same_answer_detector.lower() == "no":
+                    file.write(f"List of right answers: {right_answers_list}") #To write the list after the user exits
+                    print("Exiting...")
+                    break
                 
-        
-            right_answer = input("Input the right answer: ").lower()
+            right_answer = input("Input the right answer Ex.(A): ").lower() #Used lower() to make sure that the letter is always the same
+            right_answers_list.append(right_answer) #To store to the list
 
             file.write(f"Question {i + 1}: {question} \n") #Print the input of the user in the text file
             file.write(f"A: {option_a}\nB: {option_b}\nC: {option_c}\nD: {option_d}\n")
+            file.write(f"\n \n Right Answer: {right_answer}")
 
             i += 1 #To change the question number
+
 else:
     print(f"{quiz_number}" + " does not exist")
     print("creating the file...")
-    with open(file_path, "w") as file: #When the file does not exist, 'w' creates the file and enter 'write' mode to put texts
-        i = 0
+    with open(file_path, "w") as file: #Opens the file and using "w", it is the 'write' mode to put texts in the txt file
+        
+        i = 0 
+
+        right_answers_list = [] #To store the right answers
+
         while True: #To make it a loop so that the user can put how much questions they want in the file
             question = str(input(f"Please input the Question no.{i + 1}, if done, type 'exit': ")) #Ask the user to input their question 
-        
+
             if question.lower() == "exit": #If the user inputs "exit", make the program break and update the file with the new inputs of the user
-                print("Edit is done")
+                file.write(f"List of right answers: {right_answers_list}") #To write the list after the user exits
+                print("Exiting...")
                 break
 
+            #Ask the user to input four options, and input the correct answer
+            option_a = input("Input option A of the question: ")
+            option_b = input("Input option B of the question: ")
+            option_c = input("Input option C of the question: ")
+            option_d = input("Input option D of the question: ")
+
+            if option_a == option_b or option_a == option_c or option_a == option_d or option_b == option_c or option_b == option_d or option_c == option_d:
+                same_answer_detector = str(input("Same answer detected! Do you want to continue? Yes/No: "))
+                if same_answer_detector.lower() == "no":
+                    file.write(f"List of right answers: {right_answers_list}") #To write the list after the user exits
+                    print("Exiting...")
+                    break
+                
+            right_answer = input("Input the right answer Ex.(A): ").lower() #Used lower() to make sure that the letter is always the same
+            right_answers_list.append(right_answer) #To store to the list
+
             file.write(f"Question {i + 1}: {question} \n") #Print the input of the user in the text file
+            file.write(f"A: {option_a}\nB: {option_b}\nC: {option_c}\nD: {option_d}\n")
+            file.write(f"\n \n Right Answer: {right_answer}")
+
             i += 1 #To change the question number
-
-
-#Ask the user to input four options, and input the correct answer
